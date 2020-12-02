f0ef92ed06763fe6df4650696df9d639fdc218fb
Adam Martin <2414054@dundee.ac.uk>
2020-12-02 10:46:49 +0000

    Working frame of class

--- Classes.py
+++ Classes.py
@@ -4,11 +4,19 @@ Created on Wed Dec  2 09:57:25 2020
 
 @author: adamt
 """
-import math
+
 class CFU():
-    def ColonyFormingUnit():
-        NumberOfColonies = int(input("NumberOfColonies"))
-        AmountOfPlatted = int(input("AmountOfPlatted"))
-        DilutionFactor = int(input("DilutionFactor"))
-        CFU= NumberOfColonies/ (AmountOfPlatted * DilutionFactor)
+    def __init__(self, NumberOfColonies, AmountOfPlatted, DilutionFactor):
+        self.NOC = NumberOfColonies
+        self.AOP = AmountOfPlatted
+        self.DF = DilutionFactor
+    def ColonyFormingUnit(self):
+        CFU= self.NOC/ (self.AOP * self.DF)
         print("CFU", CFU)
+
+
+NumberOfColonies = int(input("NumberOfColonies"))
+AmountOfPlatted = int(input("AmountOfPlatted"))
+DilutionFactor = int(input("DilutionFactor"))
+ColonyA = CFU(NumberOfColonies,AmountOfPlatted,DilutionFactor)
+ColonyA.ColonyFormingUnit()
