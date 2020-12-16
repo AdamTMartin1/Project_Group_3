42979753a27e7d7dbb713e9501b76113a1cb239a
Adam Martin <2414054@dundee.ac.uk>
2020-12-16 10:35:59 +0000

    One class

--- First Idea of the GUI and functions.py
+++ First Idea of the GUI and functions.py
@@ -4,11 +4,11 @@ Created on Wed Dec  9 10:00:04 2020
 """
 
 # Import modules ---------------
-from guizero import App, Text, PushButton
-import math
+from guizero import App, Text, PushButton                                      #Takes in the folder guizero, importing in each choicen module from that folder. Guizero is a module folder that contains the modules required to create a GUI
+import math                                                                    #Imports the module math which is used for many calculatios and contains useful things such as logs and Exponetial
 
 # Functions -------------
-class CFU():
+class Calculations():
     def ColonyFormingUnit():
         NumberOfColonies = int(app.question('Number of colonies', 'Please enter as a whole number the number of colonies'))
         AmountOfPlatted = int(input("AmountOfPlatted"))
@@ -17,23 +17,20 @@ class CFU():
         print("CFU", CFU)
         return CFU
     
-class ExponetialGrowth():
     def Exponential_Growth():
         ExpontialGrowthRate = float(input("GeometricGrowthRate"))
         Population = int(input("Population"))
         ExponentialGrowth = ExpontialGrowthRate*Population
         print("Exponential Growth", ExponentialGrowth)
         return ExponentialGrowth
-    
-class GeometricGrowthOverTime():
+
     def Geometric_Growth_Over_Time():
         NetReproductiveRate = float(input("NetReproductiveRate"))
         StartingPopulation = int(input("StartingPopulation"))
         GeometricGrowthOverTime = NetReproductiveRate*StartingPopulation
         print("Geometric_Growth_over_time",GeometricGrowthOverTime)
-        return GeometricGrowthOverTime
-        
-class Population():
+        return GeometricGrowthOverTime 
+
     def Next_Total_Population():
         PopulationAtTimeZero = int(input("Population"))
         Births = int(input("Births"))
@@ -43,8 +40,7 @@ class Population():
         NextPopulation= PopulationAtTimeZero  + Births - Deaths + Immigration - Emigration
         print("NextPopulation", NextPopulation)
         return NextPopulation
-    
-class GenerationTime():
+
     def Generation_Time():
         Time = float(input("Time"))
         NumberOfGenerations = int(input("NumberOfGenerations"))
@@ -52,22 +48,19 @@ class GenerationTime():
         print("GenerationTime", GenerationTime)
         return GenerationTime
 
-class MarkAndRecapture():
     def Mark_And_Recapture():
         NumberOfCapturedFirstTime = int(input("NumberOfCapturedFirstTime"))
         NumberOfCapturedSecondTime = int(input("NumberOfCapturedSecondTime"))
         NumberOfRecaptured = int(input("NumberOfRecaptured"))
         EstimatedPopulation = (NumberOfCapturedFirstTime*NumberOfCapturedSecondTime) / NumberOfRecaptured
         print("EstimatedPopulation", EstimatedPopulation)
-        return MarkAndRecapture
-
+        return EstimatedPopulation
 
 
 #Chem equations for the Project 
 
 # % yeild = (mass of desired products/total mass of reactants)x100
 
-class Perc_Yield():
     def Percentage_Yield():
         ActualYield = float(input("ActualYield"))
         TheoreticalYield = float(input("TheoreticalYield"))
@@ -77,15 +70,13 @@ class Perc_Yield():
 
 # pH =-log10[H+]
 
-class pH():
     def pH_H():
         Hydrogen_ion_concentration = float(input("Hydrogen_ion_concentration"))
         pH = -1* (math.log10(Hydrogen_ion_concentration))
         print("pH",pH)
-        return pH
-    
+        return pH    
 
-class pOH():
+
     def pH_OH():
         Hydroxide_ion_concentration = float(input("Hydroxide_ion_concentration"))
         pOH = -1* math.log10(Hydroxide_ion_concentration)
@@ -111,23 +102,23 @@ titlee = Text(app, "it will take you to the console")
 
 
 
-CFU_button = PushButton(app, CFU.ColonyFormingUnit, text="Colony Forming Unit")
+CFU_button = PushButton(app, Calculations.ColonyFormingUnit, text="Colony Forming Unit")
 
-EG_button = PushButton(app, GeometricGrowthOverTime.Geometric_Growth_Over_Time, text="Geometric Growth Over Time")
+EG_button = PushButton(app, Calculations.Geometric_Growth_Over_Time, text="Geometric Growth Over Time")
 
-GG_button = PushButton(app, ExponetialGrowth.Exponential_Growth, text="Exponetial Growth")
+GG_button = PushButton(app, Calculations.Exponential_Growth, text="Exponetial Growth")
 
-P_button = PushButton(app, Population.Next_Total_Population, text="Next Total Population Growth")
+P_button = PushButton(app, Calculations.Next_Total_Population, text="Next Total Population Growth")
 
-GT_button = PushButton(app, GenerationTime.Generation_Time, text="Generation Time")
+GT_button = PushButton(app, Calculations.Generation_Time, text="Generation Time")
 
-MAR_button = PushButton(app, MarkAndRecapture.Mark_And_Recapture, text="Mark And Recapture")
+MAR_button = PushButton(app, Calculations.Mark_And_Recapture, text="Mark And Recapture")
 
-PY_button = PushButton(app, Perc_Yield.Percentage_Yield, text="Percentage Yeild")
+PY_button = PushButton(app, Calculations.Percentage_Yield, text="Percentage Yeild")
 
-PY_button = PushButton(app, pH.pH_H, text="pH")
+PY_button = PushButton(app, Calculations.pH_H, text="pH")
 
-PY_button = PushButton(app, pOH.pH_OH, text="pOH")
+PY_button = PushButton(app, Calculations.pH_OH, text="pOH")
 
 #print(top_text)
 # Display ---------------
